e981beaa246e4ca91303e78f44a5904f7243aab7
nickB <nbalderas524@icloud.com>
2022-11-29 14:51:41 -0800

    Proper setup via Unity editor

--- Card.cs
+++ Card.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Card : MonoBehaviour
 {
-    public int currentHealth;
-    
+    public int currentHealth, attackPower, manaCost;
+
+    public TMP_Text healthText, attackText, costText;
+
     // Start is called before the first frame update
     void Start()
     {
+        healthText = currentHelath;
         
     }
 
