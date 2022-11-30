01cfa9f1e7a9522765a3b10266256985bbed9f23
nickB <nbalderas524@icloud.com>
2022-11-29 16:30:49 -0800

    Completed creating a scriptable object and loading data from the scriptable object.

--- Card.cs
+++ Card.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    public CardScriptableObject cardSO;
+    
     public int currentHealth, attackPower, manaCost;
 
-    public TMP_Text healthText, attackText, costText;
+    public TMP_Text healthText, attackText, costText, nameText, actionDescription, loreText;
+
+    public Image characterArt, bgArt;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthText = currentHelath;
+        SetupCard();
+    }
+
+    public void SetupCard()
+    {
+        currentHealth = cardSO.currentHealth;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
         
+        healthText.text = currentHealth.ToString();
+        attackText.text = attackPower.ToString();
+        costText.text = manaCost.ToString();
+
+        nameText.text = cardSO.cardName;
+        actionDescription.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
     }
 
     // Update is called once per frame
@@ -21,4 +43,6 @@ public class Card : MonoBehaviour
     {
         
     }
+    
+    
 }
