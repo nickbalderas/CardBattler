0652189c0094a225c6411c4527ff26a8b6f1910a
nickB <nbalderas524@icloud.com>
2022-12-01 17:47:50 -0800

    Spending mana

---
+++ BattleController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public static BattleController Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int startingMana = 4, maxMana = 12;
+    public int playerMana;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMana = startingMana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpendPlayerMana(int amountToSpend)
+    {
+        playerMana -= amountToSpend;
+
+        if (playerMana < 0)
+        {
+            playerMana = 0;
+        }
+    }
+}
