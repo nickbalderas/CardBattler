0f8d3916b16d36c56a5ac0cce5fe3e47055568f4
nickB <nbalderas524@icloud.com>
2022-12-01 16:30:15 -0800

    Creating card placement areas

---
+++ CardPlacePoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlacePoint : MonoBehaviour
+{
+    public Card activeCard;
+    public bool isPlayerPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
